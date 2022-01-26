dc8fa7c66beca2c25d14c2408a6fa002ffdd9ba1
vigneshwaran-tech <83588204+vigneshwaran-tech@users.noreply.github.com>
2022-01-26 12:25:32 +0530

    saaaadd

---
+++ src/views/About.vue
@@ -0,0 +1,52 @@
+<template>
+   <main>
+     <nav>
+      <v-toolbar flat app>
+        <v-toolbar-side-icon @click="drawer = !drawer" class="grey--text"></v-toolbar-side-icon>
+        <v-toolbar-title class="text-uppercase grey--text">
+          <span class="font-weight-light">Todo</span>
+          <span>Ninja</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn flat color="grey">
+          <span>Sign Out</span>
+          <v-icon right>dashboard</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-navigation-drawer app v-model="drawer" class="primary">
+        <v-list>
+          <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-tile-action>
+              <v-icon class="white--text">{{ link.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+     
+       </nav>
+   </main>
+
+</template>
+
+<script>
+export default {
+name:"About",
+data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: 'dashboard', text: 'Dashboard', route: '/' },
+        { icon: 'folder', text: 'My Projects', route: '/Home' },
+        { icon: 'person', text: 'Team', route: '/Home' },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <main class="my-5">
+<v-btn class="blue orange--text">
+          press me
+      </v-btn>
+      <p color="red orange--text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, iusto. Consectetur odio praesentium quasi esse illum sed enim quas doloribus impedit autem sequi voluptatibus libero, accusamus nobis veniam expedita qui.</p>
+ <h1 class="red orange--text">Massive display text</h1> 
+ <p class="captialize">this is a caption</p>
+
+  <h1>This is the homepage</h1>
+    <v-divider></v-divider>
+<v-card>
+  
+      <v-btn class=" my-5">click me</v-btn>
+  <v-spacer></v-spacer>
+      <v-btn class="hidden-md-and-up">click here</v-btn>
+  <v-spacer></v-spacer>
+  
+      <v-btn class="hidden-sm-only">click out</v-btn>
+</v-card>
+
+<v-btn class="brown orange--text">
+  <v-icon left>email</v-icon>
+  hello
+</v-btn>
+ 
+
+  </main>
+</template>
+
+<script>
+export default {
+name:"Home"
+}
+</script>
+
+<style>
+
+</style>
